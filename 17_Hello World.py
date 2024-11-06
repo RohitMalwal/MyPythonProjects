d05c9d90f6060fe83c454983bd3bc3b5c97d818d
Rohit Malwal <rohitmalwal20@gmail.com>
2024-11-06 20:03:32 +0530

    saved 17_Hello World with no change

---
+++ 17_Hello World.py
@@ -0,0 +1,18 @@
+from random import choice
+from time import sleep
+
+letters_L = "abcdefghijklmnopqrstuvwxyz"
+letters_U = letters_L.upper()
+letters = letters_L + " " + letters_U
+
+word = input("Enter the word you wanna create: ")
+result = ""
+
+for letter in word:
+    while True:
+        l = choice(letters)
+        print(result+l)
+        if l==letter:
+            result += l
+            break
+        sleep(0.01)
