35c54b88dd35976aeb637032421098b755751031
Rohit Malwal <rohitmalwal20@gmail.com>
2024-11-26 18:17:33 +0530

    added images for hand dectector project

--- Hand Dectector/12_Hand Dectector.py
+++ Hand Dectector/12_Hand Dectector.py
@@ -1,7 +1,5 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-import os
-os.chdir('D:/')
 
 detector = HandDetector(maxHands=1, detectionCon=0.8)
 video = cv2.VideoCapture(0)
@@ -10,23 +8,23 @@ while True:
     _, img = video.read()
     img = cv2.flip(img, 1)
     hand, img = detector.findHands(img, draw=False)
-    fing = cv2.imread('fingers/Default.jpeg')
+    fing = cv2.imread('data/Default.jpeg')
     if hand:
         lmlist = hand[0]
         if lmlist:
             fingerup = detector.fingersUp(lmlist)
             if fingerup == [0, 0, 0, 0, 0]:
-                fing = cv2.imread("fingers/0_fingers up.jpeg")
+                fing = cv2.imread("data/0_fingers up.jpeg")
             if fingerup == [0, 1, 0, 0, 0]:
-                fing = cv2.imread("fingers/1_fingers up.jpeg")
+                fing = cv2.imread("data/1_fingers up.jpeg")
             if fingerup == [0, 1, 1, 0, 0]:
-                fing = cv2.imread("fingers/2_fingers up.jpeg")
+                fing = cv2.imread("data/2_fingers up.jpeg")
             if fingerup == [0, 1, 1, 1, 0]:
-                fing = cv2.imread("fingers/3_fingers up.png")
+                fing = cv2.imread("data/3_fingers up.png")
             if fingerup == [0, 1, 1, 1, 1]:
-                fing = cv2.imread("fingers/4_fingers up.jpg")
+                fing = cv2.imread("data/4_fingers up.jpg")
             if fingerup == [1, 1, 1, 1, 1]:
-                fing = cv2.imread("fingers/5_fingers up.jpeg")
+                fing = cv2.imread("data/5_fingers up.jpeg")
     if fing is not None:
         # Resize the image to fit in the display area
         fing = cv2.resize(fing, (220, 280))
